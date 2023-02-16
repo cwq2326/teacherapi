7371373c2d9842fe51477cfe729e898ebacd6650
cwq2326 <e0556260@u.nus.edu>
2023-02-16 18:11:53 +0800

    Update main path

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"govtech/pkg/server/database"
+	"govtech/pkg/server/handlers"
+)
+
+var dbConfig database.MySqlConfig
+var routerConfig handlers.RouterConfig
+
+func init() {
+	// Init env
+	err := godotenv.Load(filepath.Join("..", "..", ".env"))
+	if err != nil {
+		fmt.Println("Failed to load .env file")
+	}
+
+	dbConfig = database.MySqlConfig{
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASS"),
+		Port:     os.Getenv("DB_PORT"),
+		Host:     os.Getenv("DB_HOST"),
+		Name:     os.Getenv("DB_NAME"),
+	}
+
+	routerConfig = handlers.RouterConfig{
+		Port: os.Getenv("ROUTER_PORT"),
+		Host: os.Getenv("ROUTER_HOST"),
+	}
+
+	deployPort := os.Getenv("HTTP_PLATFORM_PORT")
+	if (deployPort != "") {
+		routerConfig.Port = deployPort
+	}
+}
+
+func main() {
+	// Init database.
+	db := database.ConnectDB(&dbConfig)
+	database.InitDB(db)
+	defer database.DisconnectDB(db)
+
+	// Init router.
+	r := handlers.InitRouter()
+	handlers.RegisterEndpoints(r, db)
+	handlers.RunRouter(r, &routerConfig)
+}
